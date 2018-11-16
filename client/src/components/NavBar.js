a60a1cea705f960017568b43cffd9341b50f34c6
Rguy <rguy1290@gmail.com>
2018-11-16 10:51:16 -0500

    installed styled-components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const NavBarStyles = styled.div`
-*{
-    margin: 0;
-}
-display: flex;
-padding: 10px;
-justify-content: space-around;
-background: black;
-font-weight: 800;
-color: turquoise;
-font-size: 25px;
-margin: 0;
+// const NavBarStyles = styled.div`
+// *{
+//     margin: 0;
+// }
+// display: flex;
+// padding: 10px;
+// justify-content: space-around;
+// background: black;
+// font-weight: 800;
+// color: turquoise;
+// font-size: 25px;
+// margin: 0;
 
-a{
-    text-decoration: none;  
-}
-a:visited{
-    color: turquoise;
-}
-a:hover{
-    color: white;
-}
-`
+// a{
+//     text-decoration: none;  
+// }
+// a:visited{
+//     color: turquoise;
+// }
+// a:hover{
+//     color: white;
+// }
+// `
 
 
 
 class NavBar extends Component {
     render() {
       return (
-        <NavBarStyles>
+        // <NavBarStyles>
             <div>
           <Link to="/"> 🏠 Home</Link>
           </div>
           
-        </NavBarStyles>
+        // </NavBarStyles>
       );
     }
   }
