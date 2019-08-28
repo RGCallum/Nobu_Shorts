8055a394e2e6bde9fba3a2d3c6d32a1ae24c7776
INDIA GUY <rguy129@yahoo.com>
2019-08-28 02:04:37 -0400

    added new app.get

--- server.js
+++ server.js
@@ -32,8 +32,16 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/client/build/'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/client/build/index.html')
+// })
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 
 app.use('/', routes)
